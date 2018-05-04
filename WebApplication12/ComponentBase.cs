195f572e816cac51eb3c897aca7590b518d0fc14
SaS <sas@dotnetfabrik.de>
2018-05-04 17:01:46 +0200

    Sample project with modification for Blazor Issue 759 - Repro

---
+++ WebApplication12/ComponentBase.cs
@@ -0,0 +1,100 @@
+﻿// <copyright file="Program.cs" company="DotNetFabrik">
+//     Copyright (c) DotNetFabrik － All rights reserved.
+// </copyright>
+
+namespace WebApplication12
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Threading.Tasks;
+	using Microsoft.AspNetCore.Blazor.Components;
+	using Microsoft.AspNetCore.Blazor.RenderTree;
+
+	public class ComponentBase : BlazorComponent
+	{
+		private string prefix;
+		private bool debugLog = true;
+		 
+		public ComponentBase()
+		{
+			this.prefix = $"BASE({this.GetType().Name}) ENTER";
+		}
+
+		protected override void BuildRenderTree(RenderTreeBuilder builder)
+		{
+			base.BuildRenderTree(builder);
+		}
+
+		protected new void StateHasChanged()
+		{
+			Program.Log(this.prefix);
+			base.StateHasChanged();
+		}
+
+		protected override void OnInit()
+		{
+			if ( this.debugLog )
+				Program.Log(this.prefix);
+
+			base.OnInit();
+		}
+
+		protected override void OnAfterRender()
+		{
+			if ( this.debugLog )
+				Program.Log(this.prefix);
+
+			this.isChanged = false;
+			base.OnAfterRender();
+		}
+
+		protected override Task OnAfterRenderAsync()
+		{
+			if ( this.debugLog )
+				Program.Log(this.prefix);
+
+			return base.OnAfterRenderAsync() ?? Task.CompletedTask;
+		}
+
+		protected override Task OnInitAsync()
+		{
+			if ( this.debugLog )
+				Program.Log(this.prefix);
+
+			return base.OnInitAsync() ?? Task.CompletedTask;
+		}
+
+		protected override void OnParametersSet()
+		{
+			if ( this.debugLog )
+				Program.Log(this.prefix);
+
+			base.OnParametersSet();
+		}
+
+		protected override Task OnParametersSetAsync()
+		{
+			if ( this.debugLog )
+				Program.Log(this.prefix);
+
+			return base.OnParametersSetAsync() ?? Task.CompletedTask;
+		}
+
+		public override void SetParameters(ParameterCollection parameters)
+		{
+			if ( this.debugLog )
+				Program.Log(this.prefix);
+
+			base.SetParameters(parameters);
+		}
+
+		protected override bool ShouldRender()
+		{
+			if ( this.debugLog )
+				Program.Log(this.prefix);
+
+			return base.ShouldRender();
+		}
+	}
+}
---
+++ WebApplication12/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Blazor.Browser.Rendering;
+using Microsoft.AspNetCore.Blazor.Browser.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace WebApplication12
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new BrowserServiceProvider(services =>
+            {
+                // Add any custom services here
+            });
+
+            new BrowserRenderer(serviceProvider).AddComponent<App>("app");
+        }
+
+		public static void Log(string message, [CallerMemberName] string member = null, [CallerFilePath] string filePath = null, [CallerLineNumber] int line = 0)
+		{
+			//var file = Path.GetFileName(filePath);
+			var file = filePath.Contains(@"\") ? filePath.Substring(filePath.LastIndexOf('\\') + 1) : filePath;
+			Console.WriteLine($"{message} - {member} @ {file}:{line}");
+		}
+	}
+}
